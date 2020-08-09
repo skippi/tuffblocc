6332e2381abbfe4fbe8bb51f43658a3f82d31f5c
skippi <jbtcao@gmail.com>
2020-08-09 14:10:37 -0500

    Initial commit

---
+++ src/main/java/io/github/skippi/TuffBloccMod.java
@@ -0,0 +1,24 @@
+package io.github.skippi;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.event.world.ExplosionEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = "tuffblocc", name = "tuffblocc", version = "1.0")
+public class TuffBloccMod {
+  private static Logger logger;
+
+  @EventHandler
+  public void preInit(FMLPreInitializationEvent event) {
+    logger = event.getModLog();
+  }
+
+  @EventHandler
+  public void init(FMLInitializationEvent event) {
+    logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+  }
+}
